948113b974c099e043ff590bf919ca55947d77e0
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-21 21:40:21 -0400

    set up

--- books_app/forms.py
+++ books_app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SelectField, SubmitField
-from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
+from wtforms_sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
 from wtforms.validators import DataRequired, Length, ValidationError
 from books_app.models import Audience, Book, Author, Genre
 
